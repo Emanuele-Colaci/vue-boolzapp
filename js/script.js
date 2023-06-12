697c8b3d48e8f75dacf5c74c2f397453481cd340
Emanuele-colaci <emanuele.colaci@gmail.com>
2023-06-12 16:55:38 +0200

    Fatta parte estetica

--- js/script.js
+++ js/script.js
@@ -106,7 +106,7 @@ createApp({
                 },
                 {
                     name: 'Claudia',
-                    avatar: './img/avatar_5.jpg',
+                    avatar: './img/avatar_6.jpg',
                     visible: true,
                     messages: [
                         {
