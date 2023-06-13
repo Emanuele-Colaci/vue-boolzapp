a2bc6835678ab14ddfc9975157834bc0ae682e85
Emanuele-colaci <emanuele.colaci@gmail.com>
2023-06-13 14:18:29 +0200

    Migliorato aspetto grafico

--- js/script.js
+++ js/script.js
@@ -245,5 +245,9 @@ createApp({
             
             message.showDropdown = !message.showDropdown;
         },
+        date(){
+            const date = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit'})
+            return date
+        }
     },
 }).mount('#app');
